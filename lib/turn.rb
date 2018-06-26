433ea6e5fa2df993776cc80d442d15abab670f8e
Abhishek Pathak <pathakabhishek0003@gmail.com>
2018-06-26 14:32:19 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,9 @@
+# below given method is display_board
+def display_board(board)
+  puts "board[0]|board[1]|board[2]"
+  puts "-----------"
+  puts "board[3]|board[4]|board[5]"
+  puts "-----------"
+  puts "board[6]|board[7]|board[8]"
+  puts "-----------"
+end
